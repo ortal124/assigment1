01b14639cd1d43e00e3b50fb84d2f8eeaa2f1c45
Amit Ofeck <Amitofeck1@gmail.com>
2024-11-22 12:55:11 +0200

    update post

--- routes/postRoutes.js
+++ routes/postRoutes.js
@@ -5,6 +5,7 @@ const postController = require('../controller/postController');
 router.post('/', postController.createPost);
 router.get('/', postController.getPosts);
 router.get('/:postId', postController.getPostById);
+router.put('/:postId', postController.updatePost);
 
 module.exports = router;
 
