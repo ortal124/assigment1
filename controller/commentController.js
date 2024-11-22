412c1ef6269bd39b6ebe233d3192f8f8afcbab47
Amit Ofeck <Amitofeck1@gmail.com>
2024-11-22 13:53:28 +0200

    change return status

--- controller/commentController.js
+++ controller/commentController.js
@@ -52,7 +52,7 @@ const deleteComment = async (req, res) => {
     await commentService.deleteComment(commentId); 
     res.status(200).json({ message: 'Comment deleted successfully' }); 
   } catch (error) {
-    res.status(404).json({ message: 'Error deleting comment', error: error.message });
+    res.status(500).json({ message: 'Error deleting comment', error: error.message });
   }
 };
 
